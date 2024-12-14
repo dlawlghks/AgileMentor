79e5a2ff5e5a99c95e7d01ba59cfa2416b865cf6
dlawlghks <ikuwlghks@naver.com>
2024-12-15 00:17:18 +0900

    df

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   presets: ['react-app'],
-  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  plugins: [
+    '@babel/plugin-proposal-private-property-in-object', // 플러그인 추가
+  ],
+  ignore: [
+    'node_modules/**', // 특정 경로 무시
+    'build/**',        // 빌드 파일 무시 (선택 사항)
+  ],
 };
